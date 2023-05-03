8459d8df4f8398097642f8e2756f376c456adc8a
MarinaCardozo <marina.cardozo@best2bee.com.br>
2023-05-02 23:39:46 -0300

    Projeto 02 - Aula 3 (Implementando as rotas)

--- 02-api-rest-node.js/src/server.ts
+++ 02-api-rest-node.js/src/server.ts
@@ -1,19 +1,14 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import crypto from 'node:crypto'
 import { env } from '../env'
+import { transactionsRoutes } from './routes/transactions'
+import fastifyCookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-  return transactions
+app.register(fastifyCookie)
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
