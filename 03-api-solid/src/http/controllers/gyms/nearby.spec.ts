e2442da4336ee1998944c71ccce72239ecad0276
MarinaCardozo <marina.cardozo@best2bee.com.br>
2023-08-15 00:41:16 -0300

    Projeto 03 - Aula 6 (Controllers & Testes E2E) - parte 2

---
+++ 03-api-solid/src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,55 @@
+import request from 'supertest'
+import { app } from '../../../app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '../../../utils/test/create-and-authenticate-user'
+
+describe('Nearby Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Far Gym',
+        description: null,
+        phone: null,
+        latitude: -22.4283879,
+        longitude: -43.1363774,
+      })
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Near Gym',
+        description: null,
+        phone: null,
+        latitude: -22.5129092,
+        longitude: -43.2178384,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -22.5129092,
+        longitude: -43.2178384,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Near Gym',
+      }),
+    ])
+  })
+})
